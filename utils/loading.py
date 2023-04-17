3853338a6490ce4ed6fa7c6cc0f287c1866d7dca
Marco Tacke <marco.tacke@quantco.com>
2023-04-17 12:22:23 -0700

    Small loading wrapper

---
+++ utils/loading.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+DATA_DIR = 'CTR3_tables'
+DATA_FILE_EXTENSION = '.txt'
+
+def load_table(filename:str, date_cols=[], nrows=None):
+    if not DATA_FILE_EXTENSION in filename: 
+        filename+=DATA_FILE_EXTENSION
+
+    df = pd.read_csv(
+        filepath_or_buffer=os.path.join("..",DATA_DIR, filename),
+        sep='|',
+        true_values=['Yes'],
+        false_values=['No'],
+        nrows=nrows,
+        date_format='%Y-%m-%d %H:%M:%S',
+        parse_dates=date_cols
+    )
+
+    return df
+
