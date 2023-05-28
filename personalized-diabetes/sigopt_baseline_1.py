47095a9543637e811ca5e11fa277683be59b3d7a
Johannes Fuest <johannes.fuest@tum.de>
2023-05-28 13:40:50 -0700

    debugging cloud issues

--- personalized-diabetes/sigopt_baseline_1.py
+++ personalized-diabetes/sigopt_baseline_1.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
             dict(name="dropout_rate", type="double", bounds=dict(min=0.0, max=0.5)),
             dict(name="learning_rate", type="double", bounds=dict(min=0.00001, max=0.01)),
             dict(name='num_epochs', type="int", bounds=dict(min=0, max = 20)),
-            dict(name='batch_size', type = "int", bounds=dict(min=4, max=32)),
+            dict(name='batch_size', type = "int", bounds=dict(min=1, max=4)),
             dict(name='filter_1', type = "int", bounds=dict(min=1, max=10)),
             dict(name='kernel_1', type="int", bounds=dict(min=5, max=10)),
             dict(name='stride_1', type="int", bounds=dict(min=1, max=2)),
