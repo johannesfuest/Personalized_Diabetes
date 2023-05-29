1d22d2bcccbee11d2af9607c931953061c1f0c6a
Johannes Fuest <johannes.fuest@tum.de>
2023-05-29 12:25:20 -0700

    testing baseline 1

--- personalized-diabetes/sigopt_baseline_3.py
+++ personalized-diabetes/sigopt_baseline_3.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
             dict(name="dropout_rate", type="double", bounds=dict(min=0.0, max=0.5)),
             dict(name="learning_rate", type="double", bounds=dict(min=0.00001, max=0.01)),
             dict(name='num_epochs', type="int", bounds=dict(min=1, max = 10)),
-            dict(name='batch_size', type = "int", bounds=dict(min=32, max=32)),
+            dict(name='batch_size', type = "int", bounds=dict(min=32, max=64)),
             dict(name='filter_1', type = "int", bounds=dict(min=1, max=10)),
             dict(name='kernel_1', type="int", bounds=dict(min=5, max=10)),
             dict(name='stride_1', type="int", bounds=dict(min=1, max=2)),
