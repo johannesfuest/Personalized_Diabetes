f1fb4039e56fbe70116b143421bf00b0db449954
Johannes Fuest <johannes.fuest@tum.de>
2023-06-03 21:06:36 -0700

    adding files

--- personalized-diabetes/sigopt_baseline_2.py
+++ personalized-diabetes/sigopt_baseline_2.py
@@ -133,8 +133,8 @@ def load_data_train_model(run, data, CONV_INPUT_LENGTH, write_preds=False):
     if write_preds:
         if not os.path.exists('preds'):
             os.mkdir('preds')
-        train_preds.to_csv(os.path.join('preds', f'base_2_train_M{run.params.missingness_modulo}.csv'))
-        test_preds.to_csv(os.path.join('preds', f'base_2_test_M{run.params.missingness_modulo}.csv'))
+        train_preds.to_csv(os.path.join('preds', f'base_7_train_M{run.params.missingness_modulo}.csv'))
+        test_preds.to_csv(os.path.join('preds', f'base_7_test_M{run.params.missingness_modulo}.csv'))
 
     # log performance metrics
     run.log_metric("train gMSE", train_gmse)
