3da68d2676ed230f488ba03d9e400af0d0013c00
Johannes Fuest <johannes.fuest@tum.de>
2023-06-12 16:48:25 -0700

    fixing seed issue

--- personalized-diabetes/sigopt_baseline_3.py
+++ personalized-diabetes/sigopt_baseline_3.py
@@ -24,8 +24,6 @@ def set_global_determinism(seed=SEED):
     os.environ['TF_DETERMINISTIC_OPS'] = '1'
     os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
 
-    tf.config.threading.set_inter_op_parallelism_threads(1)
-
 
 # Set seeds for reproducibility
 set_global_determinism(seed=SEED)
